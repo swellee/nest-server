a7bcf4a6877922de488fce0659ce57442ab402da
Will <will>
2022-04-20 18:13:41 +0800

    ttt

--- aws_deploy/pm2.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps: [
-        {
-            name: 'fma-server',
-            script: "./main.js"
-        }
-    ]
-}
--- aws_deploy/restart.sh
+++
@@ -1,1 +0,0 @@
-pm2 startOrRestart /home/ubuntu/server/pm2.js
