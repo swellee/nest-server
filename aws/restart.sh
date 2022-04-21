f2b3c47a63ba698b64b784a19762e51ca5585734
Will <will>
2022-04-21 15:45:16 +0800

    gg

--- aws/restart.sh
+++ aws/restart.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/sh
 cd;
-sh .bashrc;
-pm2 startOrRestart server/pm2.js
-echo server restarted at `date` >> .server_restart_log
+sh .bashrc; #load node/pm2 to path
+pm2 startOrRestart server/pm2.js;
+if [ $? = 0]; then
+    echo server restarted at `date` >> .server_restart_log;
+    exit 0;
+else
+    echo server restart fail >> .server_restart_log;
+    exit 1;
+fi;
