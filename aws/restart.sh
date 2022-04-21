7cf21f58aa056ab6478af07906b56c840bd2c2cb
Will <will>
2022-04-21 16:13:10 +0800

    deploy-aws

--- aws/restart.sh
+++ aws/restart.sh
@@ -3,12 +3,11 @@ cd;
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-echo `which pm2` > ddd;
 pm2 startOrRestart server/pm2.js;
 if [ $? = 0]; then
-    echo server restarted at `date` >> .server_restart_log;
+    echo server restart succeed at `date` >> .server_restart_log;
     exit 0;
 else
-    echo server restart fail >> .server_restart_log;
+    echo server restart failed at `date` >> .server_restart_log;
     exit 1;
 fi;
