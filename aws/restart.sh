b70aa836f5a1076bd07232ad6c4bf56f18194674
Will <will>
2022-04-21 14:51:59 +0800

    sds

--- aws/restart.sh
+++ aws/restart.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
 cd;
-echo `ls -l server`
+echo `ls -l server` > /home/ubuntu/ddd;
+echo `who` >> /home/ubuntu/ddd;
+echo `which pm2` >> /home/ubuntu/ddd;
 # pm2 startOrRestart server/pm2.js
