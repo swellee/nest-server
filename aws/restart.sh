925ce052880a5da271fc55359b6385b7659e1ef5
Will <will>
2022-04-21 14:58:42 +0800

    sw

--- aws/restart.sh
+++ aws/restart.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/sh
 cd;
-echo `ls -l server` > /home/ubuntu/ddd;
+echo `ll` > /home/ubuntu/ddd;
 echo `who` >> /home/ubuntu/ddd;
+echo `which bash` >> /home/ubuntu/ddd;
+echo `which sh` >> /home/ubuntu/ddd;
+export PATH=/opt/nodejs/bin:$PATH;
 echo `which pm2` >> /home/ubuntu/ddd;
+
 # pm2 startOrRestart server/pm2.js
